225d44128653ac78863c268384d52a7f57c824ca
Sudharshana VP <sudharshanavp@outlook.com>
2026-09-07 08:31:42 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        frequency = defaultdict(int)
+        for num in nums:
+            frequency[num]+=1
+            if frequency[num]>len(nums)//2:
+                return num
+        

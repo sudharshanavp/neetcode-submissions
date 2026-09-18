ea68ba31967d2c7bf990a46bc8cdd6ded6f2abba
Sudharshana VP <sudharshanavp@outlook.com>
2026-09-18 07:49:02 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        threshold = len(nums)//3
+        num1 = num2 = None
+        count1 = count2 = 0
+
+        for num in nums:
+            if num1 == num:
+                count1 += 1
+            elif num2 == num:
+                count2 += 1
+            elif count1 == 0:
+                num1 = num
+                count1 = 1
+            elif count2 == 0:
+                num2 = num
+                count2 = 0
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        count1 = count2 = 0
+
+        for num in nums:
+            if num == num1:
+                count1 += 1
+            if num == num2:
+                count2 += 1
+        
+        res = []
+        if count1 > threshold:
+            res.append(num1)
+        if count2 > threshold:
+            res.append(num2)
+        
+        return res
+        
+        
+
+        
